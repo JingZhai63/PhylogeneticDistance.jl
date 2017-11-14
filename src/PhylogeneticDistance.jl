90d28e1208d525c06057aebb144788b4182a5ce7
Jing Zhai <jingzhai.email.arizona.edu>
2017-11-13 17:40:58 -0700

    update file

--- src/PhylogeneticDistance.jl
+++ src/PhylogeneticDistance.jl
@@ -4,31 +4,31 @@ import StatsBase
 using StatsBase
 
 export
-       readNewick,
        getLabel,
        getEdgeLength,
-       node_depth,
-       foo_reorder,
-       bar_reorder,
-       neworder_phylo,
-#       reorderPhylo,
-#       DropTip,
+       readNewick,
        isRooted,
        GUniFrac
-       # ,
+#       node_depth,
+#       foo_reorder,
+#       bar_reorder,
+#       neworder_phylo,
+#       reorderPhylo,
+#       DropTip,
 #       KernelMatrix
 
-include("readNewick.jl")
 include("getLabel.jl")
 include("getEdgeLength.jl")
-include("node_depth.jl")
-include("foo_reorder.jl")
-include("bar_reorder.jl")
-include("neworder_phylo.jl")
-# include("reorderPhylo.jl")
+include("readNewick.jl")
 include("isRooted.jl")
-# include("DropTip.jl")
 include("GUniFrac.jl")
+
+# include("node_depth.jl")
+# include("foo_reorder.jl")
+# include("bar_reorder.jl")
+# include("neworder_phylo.jl")
+# include("reorderPhylo.jl")
+# include("DropTip.jl")
 # include("KernelMatrix.jl")
 
 end
